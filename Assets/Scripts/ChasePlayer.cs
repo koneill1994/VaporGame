5636072dedb6ff6a847aa3401166d3752bf7c338
Kevin O'Neill <Kevin O'Neill>
2016-06-15 15:32:29 -0400

    AI pathing, model updates

---
+++ Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,41 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class ChasePlayer : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+    private float base_speed;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        base_speed = agent.speed;    
+    }
+
+    void Update()
+    {
+        agent.destination = player.transform.position;
+
+        if (gameObject.GetComponent<Renderer>().isVisible)
+        {
+            //Debug.Log("IS VISIBLE");
+            agent.speed = 0.0f;
+            agent.velocity = Vector3.zero;
+        }
+        else
+        {
+            //NOT VISIBLE
+            agent.speed = base_speed;
+            GameObject[] main_cam = GameObject.FindGameObjectsWithTag("MainCamera");
+            if (main_cam.Length == 1)
+            {
+                
+
+            }
+        }
+
+
+    }
+}
