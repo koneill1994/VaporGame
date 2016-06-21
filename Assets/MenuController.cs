4445ad4eb69e19eb4799a199d87e42dc4de2447e
Kevin O'Neill <Kevin O'Neill>
2016-06-21 04:10:38 -0400

    minor progress on menu

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
 using System.Collections;
 using UnityEngine.SceneManagement; // neded in order to load scenes
+using System.IO;
 
 public class MenuController : MonoBehaviour
 {
@@ -13,28 +14,42 @@ public class MenuController : MonoBehaviour
     //this is so fucking confusing
     //i think i need to add onclick events to fire when buttons are pressed
     // todo figure out how that works
-
+    
     void Start()
     {
-        dropdown.ClearOptions();
-        for (int i= 0; i< SceneManager.sceneCount; i++)
+        /*
+        EditorBuildSettingsScene[] allScenes = EditorBuildSettings.scenes;
+        Debug.Log("All Scenes : Length : " + allScenes.Length);
+        string path;
+        for (int i = 0; i < allScenes.Length; i++)
         {
-            Debug.Log(i);
-            Dropdown.OptionData list = new Dropdown.OptionData(SceneManager.GetSceneAt(i).name);
+            Debug.Log("All Path : Scene : " + allScenes[i].path);
+            path = Path.GetFileNameWithoutExtension(allScenes[i].path);
+            Debug.Log("Clear Path : Scene : " + path);
+        }
+
+        dropdown.ClearOptions();
+        for (int i= 0; i< SceneManager.sceneCountInBuildSettings; i++)
+        {
+            Debug.Log(allScenes[i].path);
+            //SceneManager.LoadSceneAsync(i);
+            Dropdown.OptionData list = new Dropdown.OptionData(allScenes[i].path);
+            dropdown.options.Add(list);
+        }*/
+
+        dropdown.ClearOptions();
+        string[] SceneNames = new string[] { "Levels_Test_1", "Test_Game", "Door Test", "Obstacle_Course"};
+
+        foreach (string name in SceneNames)
+        {
+            Dropdown.OptionData list = new Dropdown.OptionData(name);
             dropdown.options.Add(list);
         }
-        
+
         quitMenu = quitMenu.GetComponent<Canvas>();
         startText = startText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
         quitMenu.enabled = true;
-
-    }
-
-    void Update()
-    {
-        //onClick
-
 
     }
     
@@ -57,9 +72,9 @@ public class MenuController : MonoBehaviour
     }
 
     public void StartLevel() //this function will be used on our Play button
-
     {
-        SceneManager.LoadScene(dropdown.value); //this will load our first level from our build settings. "1" is the second scene in our game
+        Debug.Log(dropdown.captionText.text);
+        //SceneManager.LoadScene(dropdown.captionText.text); //this will load our first level from our build settings. "1" is the second scene in our game
 
     }
 
