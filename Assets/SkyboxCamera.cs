1de45d2086a0ee51a3a44bceb26e8d117c73d674
Kevin O'Neill <Kevin O'Neill>
2016-11-04 21:29:40 -0400

    Fixed jitter through the wonders of LateUpdate

--- Assets/SkyboxCamera.cs
+++ Assets/SkyboxCamera.cs
@@ -42,7 +42,7 @@ public class SkyboxCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Debug.Log("1: " + MainCamera.transform.rotation.eulerAngles);
 
