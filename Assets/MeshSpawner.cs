c03607311b96b53181b6614d32da4166ca16cebd
Kevin O'Neill <Kevin O'Neill>
2016-11-12 01:42:03 -0500

    cleaning up code a bit

--- Assets/MeshSpawner.cs
+++ Assets/MeshSpawner.cs
@@ -3,26 +3,24 @@ using System.Collections;
 
 public class MeshSpawner : MonoBehaviour {
 
-    public int width;
+    public int width;           //width and height of mesh to make  (make vector2 later for simplicity's sake)
     public int height;
 
-    public float heightscale;
+    public float heightscale;   // perlin noise gives values [0,1], this multiplies to get a height
 
-    public float NoiseScale;
-    public Vector2 NoiseOffset;
+    public float NoiseScale;    //multiplies to scale the noise; larger number is finer noise
+    public Vector2 NoiseOffset; //randomize values to get different noisemaps; basically translates the noisemap presented to get different noise
 
     private Mesh mesh;
-
-    // Use this for initialization
+    
     void Start () {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         mesh.vertices = MakeVertices(width, height);
         mesh.uv = MakeUVs(mesh.vertices,width,height);
         mesh.triangles = MakeTriangles(mesh.vertices,width,height);
-
-        mesh.RecalculateNormals();
         GetComponent<MeshCollider>().sharedMesh = mesh;
+        mesh.RecalculateNormals();
 
     }
 
@@ -36,7 +34,7 @@ public class MeshSpawner : MonoBehaviour {
 
         PHASE 2
         
-        make and spawn a mesh of arbitrary size, with height determined by a procgen simplex noise algorithm
+        make and spawn a mesh of arbitrary size, with height determined by a procgen noise algorithm
         //done!
 
         PHASE 3
@@ -78,56 +76,39 @@ public class MeshSpawner : MonoBehaviour {
         return uvs;
     }
 
+
+    //this returns a list of triangles (mesh.triangles) given a CARTESIAN LIST OF POINTS WITH THE GIVEN WIDTH AND HEIGHT
+    //if you use it on something else and ruin your whole day, thats your problem
     int[] MakeTriangles(Vector3[] verts, int width, int height)
     {
-        //this returns a list of triangles (mesh.triangles) given a CARTESIAN LIST OF POINTS WITH THE GIVEN WIDTH AND HEIGHT
-        //if you use it on something else and ruin your whole day, thats your problem
-
         int[] Triangles = new int[(width-1)*(height-1)*2*3];
         //for width*height number of points
         //there will be (width-1)*(height-1) number of squares between those points
         //each square is made up of 2 triangles
         //each triangle is specified by 3 integers
 
-        int m = 0;  //i can't be bothered to do it without a counter at the moment
-        //do the math later
+        int m = 0;
         for (int i = 0; i < width-1; i++)
         {
             for (int j = 0; j < height-1; j++)
             {
-                //so for each square, make two triangles
+                //for each square, make two triangles
                 //the top one
                 //    [(i,j),(i+1,j),(i+1,j+1)]
                 //    
-                Triangles[m]= i * height + j;
-                m++;
-                Triangles[m] = (i + 1) * height + (j + 1);
-                m++;
-                Triangles[m] = (i + 1) * height + j;
-                m++;
+                Triangles[m++]= i * height + j;
+                Triangles[m++] = (i + 1) * height + (j + 1);
+                Triangles[m++] = (i + 1) * height + j;
 
                 //the bottom one
                 //    [(i,j),(i,j+1),(i+1,j+1)]
-                Triangles[m] = i * height + j;
-                m++;
-                Triangles[m] = i * height + (j+1);
-                m++;
-                Triangles[m] = (i + 1) * height + (j + 1);
-                m++;
+                Triangles[m++] = i * height + j;
+                Triangles[m++] = i * height + (j+1);
+                Triangles[m++] = (i + 1) * height + (j + 1);
 
             }
         }
-        //remember, triangle list is a list of positions in the vertices list
         return Triangles;
-    }
-
-
-    int VertNumFromCoords(int i, int j)
-    {
-        //not sure if ill need this yet
-        //i * height + j = num
-        //
-        return 0;
     }
 
 }
